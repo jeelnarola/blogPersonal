445a2bc0929c867d416d102aa623ce57f8a1752a
jeelnarola <narolajeel29@gmail.com>
2024-12-06 05:02:29 +0530

    ConnectDb File Change

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config({path: "../.env"})
 const DataBaseConnect = async()=>{
-    await mongoose.connect(process.env.MONGODB_URL)
-    console.log('DataBase Connect...')
+    console.log(process.env.MONGODB_URL)
+    try{
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log('DataBase Connect...')
+
+    }catch(error){console.log({message:"DataBse Connection User :- ",error:error})}
 }
 
 module.exports = {DataBaseConnect}
