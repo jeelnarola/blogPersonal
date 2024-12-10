4552bcb2d9de13c7062609d80054853b72596e98
jeelnarola <narolajeel29@gmail.com>
2024-12-10 21:47:16 +0530

    Project Done

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config({path: "../.env"})
 const DataBaseConnect = async()=>{
-    console.log(process.env.MONGODB_URL)
     try{
         await mongoose.connect(process.env.MONGODB_URL)
         console.log('DataBase Connect...')
 
-    }catch(error){console.log({message:"DataBse Connection User :- ",error:error})}
+    }catch(error){
+        console.log({message:"DataBse Connection User :- ",error:error})
+    }
 }
 
 module.exports = {DataBaseConnect}
